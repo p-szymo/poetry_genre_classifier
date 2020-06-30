6c2337fb87d39f031e767ceb88f306e15d8aeba3
“p-szymo” <joshua.szymanowski@gmail.com>
2020-06-30 09:15:14 -0400

    prepping presentation and readme

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import itertools
 
 import re
 import string
@@ -320,3 +322,37 @@ def clean_text(text, stop_words):
     text = ' '.join(words)
     
     return text
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Code modified from work by SeanAbu Wilson.
+    """
+    # convert to percentage, if normalize set to True
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    # plot
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    # format true positives and others
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), fontsize=15,
+                 horizontalalignment="center", verticalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    # add axes labels
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
